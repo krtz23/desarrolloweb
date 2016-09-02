9e93122ca6da1662b65642f3807662280688bb9a
krtz23 <jchronnox@gmail.com>
2016-09-01 23:49:07 -0300

    Add files via upload

--- herramientas de desarrollo web/javascripts.js
+++ herramientas de desarrollo web/javascripts.js
@@ -87,30 +87,31 @@ function imcXestatura(){
 	var estaturaX2=estatura*estatura;
 	var formula=p1/estaturaX2;
 
-	window.alert(formula);
+	var est=formula.toFixed(1);
 
 	if (formula < 18) {
-		window.alert(" Peso bajo. Necesario valorar signos de desnutrición");
+
+		window.alert("Su IMC corresponde a: Peso bajo. Necesario valorar signos de desnutrición");
 	} else {
 		if (formula == 18 || formula <= 24.9) {
-			window.alert(" Normal ");
+			window.alert("Su IMC corresponde a: Normal ");
 		} else {
 			if (formula == 25 || formula <= 26.9) {
-				window.alert(" Sobrepeso");
+				window.alert("Su IMC corresponde a: Sobrepeso");
 			} else {
 				if (formula == 27) {
-					window.alert(" Obesidad");
+					window.alert("Su IMC corresponde a: Obesidad");
 				} else {
 					if (formula == 27.1 || formula <= 29.9) {
-						window.alert(" Obesidad grado I. Riesgo relativo alto para desarrollar enfermedades cardiovasculares");
+						window.alert("Su IMC corresponde a: Obesidad grado I. Riesgo relativo alto para desarrollar enfermedades cardiovasculares");
 					} else {
 						if (formula == 30 || formula <= 39.9) {
-							window.alert("	Obesidad grado II. Riesgo relativo muy alto para el desarrollo de enfermedades cardiovasculares");
+							window.alert("Su IMC corresponde a:	Obesidad grado II. Riesgo relativo muy alto para el desarrollo de enfermedades cardiovasculares");
 						} else {
 							if (formula >= 40) {
-								window.alert(" Obesidad grado III Extrema o Mórbida. Riesgo relativo extremadamente alto para el desarrollo de enfermedades cardiovasculares");
+								window.alert("Su IMC corresponde a: Obesidad grado III Extrema o Mórbida. Riesgo relativo extremadamente alto para el desarrollo de enfermedades cardiovasculares");
 							} else {
-								window.alert(" Ingrese su 'PESO' y 'ESTATURA'.Por favor");
+								window.alert("Su IMC corresponde a: Ingrese su 'PESO' y 'ESTATURA'.Por favor");
 							}
 						}
 					}
@@ -118,4 +119,23 @@ function imcXestatura(){
 			}
 		}
 	}
+
+    window.alert(" Su peso es: "+p1 + "Kg. , Su estatura es : "+ estatura +"M. , Su IMC cooresponde a : "+est+". Acontinuación su indice de grasa:");
+    var sexo=document.getElementById("sexo").value;
+    var edad=parseInt(document.getElementById("edad").value);
+	var opt=parseInt(sexo);
+switch (opt) {
+    case 1:
+    	var resulgH=(1.20*est)+(0.23*edad)-(10.8*1)-5.4;
+        window.alert("Si eres 'Hombre' tu indice de grasa aproximado es: "+resulgH.toFixed(2)+"%");
+        break;
+    case 0:
+       var resulgM=(1.20*est)+(0.23*edad)-(10.8*0)-5.4;
+        window.alert("Si eres 'Mujer' tu indice de grasa aproximado es: "+resulgM.toFixed(2)+"%");
+        break;
+    default:
+     window.alert("Sin tu edad y sexo no podemos calcular tu indice de grasa");
+}
+
+
 }
